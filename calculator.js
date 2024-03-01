07e450192a322a5c7df70aa30b7606316008f85b
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-03-01 14:26:56 -0800

    An

---
+++ calculator.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('monthlyIncome').addEventListener(change, function(eventData)){
+       documentElement.getElement
+    }
+});
